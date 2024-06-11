78caef0a2ac7778540c8997ed45a217e131e2cf4
[gunryul] <[gun9311@gmail.com]>
2024-06-11 14:40:09 +0900

    modify crawling time

--- src/fit-style-backend/src/main/java/ru/project/fitstyle/service/impl/FitCrawlingService.java
+++ src/fit-style-backend/src/main/java/ru/project/fitstyle/service/impl/FitCrawlingService.java
@@ -55,7 +55,7 @@ public class FitCrawlingService implements CrawlingService {
     }
 
     @Override
-    @Scheduled(cron = "30 41 11 * * ?")
+    @Scheduled(cron = "00 00 03 * * ?")
     public List<Article> crawling() {
         List<Article> articles = new ArrayList<>();
         ChromeOptions options = new ChromeOptions();
